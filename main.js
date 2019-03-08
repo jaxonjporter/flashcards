8a9630164e2d454c13fe9e118bf3f54c68865c29
Jaxon Porter <jaxonutah@gmail.com>
2019-03-08 16:42:32 -0700

    got the card to flip

--- main.js
+++ main.js
@@ -1,22 +1,38 @@
 $(document).ready( function() {
 
-  var flashCards = []
-  var info = {}
-  
-  for (var i = 0; i < flashCards.length)
+  var flashCards = [{front: "Function", back: "A block of code that does a specific task"}, {front: "Class", back: "Code that defines an object"}]
+  var flag = false
+
+  flashCards.forEach( function(char) {
+    var li = '<li class="list-group-item item">' + char.front + '</li>'
+    $('#list').append(li)
+  });
+
 
   $("#list").on("click", function(event) {
-    var item = event.target.tagName
-    var stuff = event.target.innertext
-    card
+
+    let item = event.target.tagName
+    console.log(item)
+    let stuff = event.target.innerText
     if (item === "LI") {
-      $("#test").text(stuff.innerText)
-      info.front = stuff
+      flashCards.filter( function(name){
+        if(name.front === stuff){
+
+          $("#test").text(name.front)
+        }
+      })
     } else {
       $("#test").text("It Doesnt Work")
     }
   })
 
-  
+  $(document).on('click', "#post", function() {
+    let s = test.innerText
+    flashCards.filter( function(name){
+      if(name.front === s){
+        $("#test").text(name.back)
+      }
+    })
+  })
   
 });
